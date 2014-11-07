e265ab052285eb130413c0ee8b827f577ca07d18
Phuoc Nguyen <phuoc@huuphuoc.me>
2014-11-07 08:39:15 +0700

    #1014: Fix enabling submit button example

--- _includes/examples/enabling-submit-button/example.js
+++ _includes/examples/enabling-submit-button/example.js
@@ -30,12 +30,19 @@ $(document).ready(function() {
                 }
             }
         })
-        .on('status.field.bv', function (e, data) {
+        .on('error.field.bv', function(e, data) {
             // $(e.target)  --> The field element
             // data.bv      --> The BootstrapValidator instance
             // data.field   --> The field name
             // data.element --> The field element
 
+            data.bv.disableSubmitButtons(false);
+        })
+        .on('success.field.bv', function(e, data) {
+            // e, data parameters are the same as in error.field.bv event handler
+            // Despite that the field is valid, by default, the submit button will be disabled if all the following conditions meet
+            // - The submit button is clicked
+            // - The form is invalid
             data.bv.disableSubmitButtons(false);
         });
 });
